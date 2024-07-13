af213179a2c8e753c4081f51ead6e9a887c03f71
shukla-sahil <87928951+shukla-sahil@users.noreply.github.com>
2024-07-13 23:27:45 +0530

    RazorPay Integration

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const AboutPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">About Us</h1>
+        <hr />
+        <p className="lead text-center">
+        At our store, we believe in transforming the shopping experience by offering high-quality products, exceptional customer service, and a seamless online shopping experience. Established in 2007, our mission has always been to bring the best products from around the world right to your doorstep. <br />
+        We value our customers and the community we build together. Follow us on social media to stay updated on the latest products, promotions, and news. Don’t forget to sign up for our newsletter to receive exclusive offers and discounts.
+        </p>
+
+        <h2 className="text-center py-4">Our Products</h2>
+        <div className="row">
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/298863/pexels-photo-298863.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Mens's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/7679720/pexels-photo-7679720.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Women's Clothing</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/1927259/pexels-photo-1927259.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Jewelery</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-3 col-sm-6 mb-3 px-3">
+            <div className="card h-100">
+              <img className="card-img-top img-fluid" src="https://images.pexels.com/photos/356056/pexels-photo-356056.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" height={160} />
+              <div className="card-body">
+                <h5 className="card-title text-center">Electronics</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/ThankYou.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ThankYou = () => {
+  return (
+    <div className="container my-5">
+      <h1 className="text-center">Thank You for Your Order!</h1>
+      <p className="text-center">Your payment was successful.</p>
+    </div>
+  );
+};
+
+export default ThankYou;
